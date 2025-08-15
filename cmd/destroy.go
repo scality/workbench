18984cdee1a1b1a1f13520b629a13cad70251b54
Taylor McKinnon <mail@tmacs.space>
2025-08-15 11:23:07 -0700

    fix: Use errors.Is to handle wrapped errors

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -52,7 +53,7 @@ func (c *DestroyCmd) Run() error {
 	cmd.Stderr = os.Stderr
 	cmd.Dir = filepath.Join(c.EnvDir, c.Name)
 	if err := cmd.Run(); err != nil {
-		if ctx.Err() == context.Canceled {
+		if errors.Is(ctx.Err(), context.Canceled)  {
 			return nil
 		}
 		return err
